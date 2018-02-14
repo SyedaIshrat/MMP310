c8d543c52078405bf44a94c8cb6b4908961bc8f3
Syeda Ishrat <syeda.ishrat@f1006-18-34184.bmcc.cuny.edu>
2018-02-14 18:33:54 -0500

    Madlibs

---
+++ Madlibs/app.js
@@ -0,0 +1,19 @@
+const submitButton = document.getElementById("submit");
+const exclamationInput = document.getElementById("exclamation");
+const adverbInput = document.getElementById("adverb");
+const nounInput = document.getElementById("noun");
+const adjectiveInput = document.getElementById("adjective");
+
+submitButton.onclick = function() {
+	const message = document.createElement("p");
+	message.textContent = exclamationInput.value
+						+ " he said "
+						+ adverbInput.value
+						+ " as he jumped into his convertible "
+						+ nounInput.value
+						+ " and drove off with his "
+						+ adjectiveInput.value
+						+ " wife "
+						
+	document.body.appendChild(message);
+};
