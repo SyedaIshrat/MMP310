537d9dd3e67e3e3c1dc512c11027e9873422e0d8
SyedaIshrat <Rasyedais@gmail.com>
2018-05-09 21:25:22 -0400

    Add files via upload

--- Final/app.js
+++ Final/app.js
@@ -11,6 +11,33 @@ $(function() {
     });
 });
 
+function randomQuote() {
+ $.ajax({
+      url: "https://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      },
+      success: function(quote) {
+        $('#quote').html('&ldquo;'+quote.quoteText+'&rdquo;')
+        $('#author').html("-"+quote.quoteAuthor)
+      }
+    });
+}
+
+randomQuote();
+
+//Click on the button to generate another random quote
+
+$('#random').click(function() {
+  
+    randomQuote();
+   
+  })
+
 function search() {
     // clear 
     $('#results').html('');
